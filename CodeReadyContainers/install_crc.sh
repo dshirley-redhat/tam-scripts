b3357414bc1f11a56e14fa529174d92152fc1491
Derek Shirley <dshirley@redhat.com>
2023-07-04 17:08:01 -0400

    changed where the binary is installed and added a soft link to it

--- CodeReadyContainers/install_crc.sh
+++ CodeReadyContainers/install_crc.sh
@@ -1,16 +1,19 @@
 #change this directory if required
-CRC_DIR=~/crc
-VERSION=2.22.1
+CRC_DIR=~/.crc
+CRC_VERSION=2.22.1
 ARCH=amd64
 
-
-mkdir $CRC_DIR
-wget https://mirror.openshift.com/pub/openshift-v4/clients/crc/$VERSION/crc-linux-$ARCH.tar.xz
+mkdir -p $CRC_DIR
+wget https://mirror.openshift.com/pub/openshift-v4/clients/crc/$CRC_VERSION/crc-linux-$ARCH.tar.xz
 tar -xvf crc-linux-$ARCH.tar.xz
 
-mv crc-linux-$VERSION-$ARCH/* $CRC_DIR
+mv -f crc-linux-$CRC_VERSION-$ARCH/* $CRC_DIR
 rm crc-linux-$ARCH.tar.xz
-rm -r crc-linux-$VERSION-$ARCH
+rm -r crc-linux-$CRC_VERSION-$ARCH
 
 cd $CRC_DIR
 chmod +x crc
+
+ln -sf $CRC_DIR/crc ~/bin/crc
+
+crc version
