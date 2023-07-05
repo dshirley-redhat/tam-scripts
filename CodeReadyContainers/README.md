3f14f8f311402d4fb12ed7994b7a7514cc5d3268
Derek Shirley <dshirley@redhat.com>
2023-07-05 09:55:39 -0400

    made the install script more robust and added the ability to pass in a parameter for the crc version

--- CodeReadyContainers/README.md
+++ CodeReadyContainers/README.md
@@ -2,7 +2,7 @@
 
 #### Installing the crc tool
 
-Run ```./install_crc.sh``` from a terminal, this will download and install the crc installer.
+Run ```./install_crc.sh``` from a terminal, this will download and install the latest crc version.
 
 If you require a specific OCP version run ```getCrcOcpVersions.sh``` to get the compatible crc version and update ```./install_crc.sh``` with the CRC_VERSION you require.
 
--- CodeReadyContainers/install_crc.sh
+++ CodeReadyContainers/install_crc.sh
@@ -1,15 +1,32 @@
 #change this directory if required
 CRC_DIR=~/.crc
-CRC_VERSION=2.22.1
+
+usage() {
+    echo "./install_crc.sh <crc version>"
+}
+
+CRC_VERSION=latest
 ARCH=amd64
+TMP_OUTPUT_DIR=output
+
+if [ "$#" -gt 1 ]; then
+    echo "Illegal number of parameters"
+elif [ "$#" -eq 1 ]; then
+    CRC_VERSION=$1
+fi
+
+echo "installing crc version: $CRC_VERSION"
 
 mkdir -p $CRC_DIR
-wget https://mirror.openshift.com/pub/openshift-v4/clients/crc/$CRC_VERSION/crc-linux-$ARCH.tar.xz
-tar -xvf crc-linux-$ARCH.tar.xz
 
-mv -f crc-linux-$CRC_VERSION-$ARCH/* $CRC_DIR
+mkdir -p $TMP_OUTPUT_DIR
+
+wget -q https://mirror.openshift.com/pub/openshift-v4/clients/crc/$CRC_VERSION/crc-linux-$ARCH.tar.xz
+tar -xvf crc-linux-$ARCH.tar.xz -C $TMP_OUTPUT_DIR --strip-components 1
+
+mv -f $TMP_OUTPUT_DIR/* $CRC_DIR
 rm crc-linux-$ARCH.tar.xz
-rm -r crc-linux-$CRC_VERSION-$ARCH
+rm -rf $TMP_OUTPUT_DIR
 
 cd $CRC_DIR
 chmod +x crc
